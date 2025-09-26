67d5dbb502681b46e7a09b4772b2973f1a0ed720
Paul Barre Beavogui <beavoguipaulbarre047@gmail.com>
2025-09-26 01:32:21 +0000

    Ajout du sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ListeCombattants } from './components/liste-combattants/liste-combattants';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',pathMatch:'full',redirectTo:'listeCombattant'},
+    {path:'listeCombattant',component:ListeCombattants},
+    
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ListeCombattants } from './components/liste-combattants/liste-combattants';
+import { Sidebar } from './components/sidebar/sidebar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,ListeCombattants],
+  imports: [RouterOutlet,Sidebar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
