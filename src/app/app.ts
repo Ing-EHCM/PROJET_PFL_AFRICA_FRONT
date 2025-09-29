d7c0633288f5cb00f3cc9a8722dbdffd6f9a8915
Ing.EHCM <elhadjicire@gmail.com>
2025-09-29 19:37:40 +0000

    Service

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ListeCombattants } from './components/liste-combattants/liste-combattants';
 import { Sidebar } from './components/sidebar/sidebar';
+import { NgxSonnerToaster } from "ngx-sonner";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Sidebar],
+  imports: [RouterOutlet, Sidebar, NgxSonnerToaster],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
